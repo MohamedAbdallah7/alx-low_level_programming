dce51653ed27de238cea685c87848b8b687a03f2
MohamedAbdallah7 <mabdaullah7@gmail.com>
2023-03-07 11:42:47 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+
+
+void print_alphabet(void)
+{
+	char francis;
+
+	for (francis = 'a'; francis <= 'z'; francis++)
+	_putchar(francis);
+
+	_putchar('\n');
+}
